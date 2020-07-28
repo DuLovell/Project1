22f8f239605dde06decc65931c599abaf6fc5798
admin <bednyakov99@gmail.com>
2020-07-28 19:55:57 +0300

    Add Entry pages view + discover a way of how to convert Markdown to HTML

--- encyclopedia/urls.py
+++ encyclopedia/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+app_name="encyclopedia"
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    # wiki/search/ path
+    # wiki/create/ path
+    path("<str:entry>", views.entry , name="entry"),
 ]
--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseNotFound, Http404
+from markdown2 import Markdown
 from . import util
 
 
@@ -8,3 +9,11 @@ def index(request):
         "entries": util.list_entries()
     })
 
+def entry(request, entry):
+	if util.get_entry(entry):
+		return render(request, "encyclopedia/entry.html", {
+			"arcticle": Markdown().convert(util.get_entry(entry)),
+			"title": entry
+			})
+	#return HttpResponseNotFound("<h1>Page not found</h1>")
+	raise Http404("Page not found")
